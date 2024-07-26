d69a262f2577ca4b2d4ddc738f1743a17f037d5e
TheoCaesar <theocaesar7@gmail.com>
2024-07-26 05:56:28 +0100

    working with signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
 import { interval, map } from 'rxjs';
 
 @Component({
@@ -8,6 +8,12 @@ import { interval, map } from 'rxjs';
 })
 export class AppComponent implements OnInit{
   private destroy = inject(DestroyRef)
+  count = signal<number>(0);
+  constructor(){
+    effect(()=>{
+      console.log(`You clicked button ${this.count()} times...`)
+    })
+  }
   ngOnInit() {
     const interval$ = interval(1000).pipe(
       map((val) => val*2)
@@ -24,4 +30,7 @@ export class AppComponent implements OnInit{
     this.destroy.onDestroy(interval$.unsubscribe)
   }
 
+  onClick(){
+    this.count.update((counter)=>counter+=1)
+  }
 }
