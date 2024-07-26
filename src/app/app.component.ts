10c0cba8c85c38f6affecf557af2e622b4a9a801
TheoCaesar <theocaesar7@gmail.com>
2024-07-26 05:36:00 +0100

    pipe operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, DestroyRef, inject, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +9,17 @@ import { interval } from 'rxjs';
 export class AppComponent implements OnInit{
   private destroy = inject(DestroyRef)
   ngOnInit() {
-    const interval$ = interval(1000).subscribe({
+    const interval$ = interval(1000).pipe(
+      map((val) => val*2)
+    ).subscribe({
       next:(value)=>console.log(value),
-      error: (err) => console.error(err),
-      complete: ()=>console.log('RxJs Interval Observable Complete')
     })
 
     //unsubscribe
     setTimeout(() => {
       interval$.unsubscribe()
     }, 15000);
-
+ 
     // unsubscribe onDestroy of component
     this.destroy.onDestroy(interval$.unsubscribe)
   }
