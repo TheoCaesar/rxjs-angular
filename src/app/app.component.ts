6e7d42fd6687f5b5616cac002f43586f94490033
TheoCaesar <theocaesar7@gmail.com>
2024-07-26 07:31:38 +0100

    custom observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
-import { count, interval, map } from 'rxjs';
+import { count, interval, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,12 +15,30 @@ export class AppComponent implements OnInit{
   //create interval observable & convet to signal...
   intervalSignal = interval(1000)
   count$ignal = toSignal(this.intervalSignal, {initialValue:25});
+
+  customInterval = new Observable((subscriber)=>{
+    let count = 0
+    const interval = setInterval(() => {
+      if (count >2) {
+        clearInterval(interval)
+        subscriber.complete();
+        return;
+      }
+      console.log('emitting new Value...')
+      subscriber.next({message: 'New Value'})
+      count++;
+    }, 2000);
+  });
   constructor(){
     // effect(()=>{console.log(`You clicked button ${this.count()} times...`)})
   }
   ngOnInit() {
-    const subscr = this.count$.subscribe((data)=>{
-      console.log(`You clicked button ${data} times...`)
+    const custSubscr = this.customInterval.subscribe({
+      next: (val)=>console.log(val), 
+      complete: ()=> console.log('Already run 3 times...Completed!')
+    })
+    const subscr = this.count$.subscribe({
+      next: (data)=> console.log(`You clicked button ${data} times...`),
     })
   }
 
