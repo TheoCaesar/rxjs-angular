247102dc7a8ca2af7c07f32027362f1284925d43
TheoCaesar <theocaesar7@gmail.com>
2024-07-26 04:25:22 +0100

    rxjs - interval function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  private destroy = inject(DestroyRef)
+  ngOnInit() {
+    const interval$ = interval(1000).subscribe({
+      next:(value)=>console.log(value),
+      error: (err) => console.error(err),
+      complete: ()=>console.log('RxJs Interval Observable Complete')
+    })
+
+    //unsubscribe
+    setTimeout(() => {
+      interval$.unsubscribe()
+    }, 15000);
+
+    // unsubscribe onDestroy of component
+    this.destroy.onDestroy(interval$.unsubscribe)
+  }
 
 }
